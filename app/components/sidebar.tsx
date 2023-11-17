77b4621cc39b5d5e9cb05023e8008ffab1e3be5e
BestZx0521 <82031494+BestZx0521@users.noreply.github.com>
2023-11-17 14:25:25 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           麻香嘴AI
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          打造藤椒产业全球卓越供应链.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
